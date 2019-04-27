162faf4c8315f0db9a6f4c4a9134dbb85b8e6ea9
yasser <yassergeo@gmail.com>
2019-04-27 11:53:12 -0500

    Uniendo Back con Front

--- back-end/src/resolvers/Mutation.js
+++ back-end/src/resolvers/Mutation.js
@@ -1,12 +1,18 @@
 const Sala = require('../models/Sala')
 
 async function createSala(root, args) {  
-      console.log("Estoy llegando aqui")
+     
       let sala = await new Sala(args)
       sala.save()
-      return sala 
+      return sala
 }
 
+/* async function deleteSala(root, args) {  
+      //const sala  =  await Sala.findByIdAndRemove({_id: args._id})
+      //return sala
+} */
+
 module.exports = {
-      createSala
+      createSala,
+     // deleteSala
 }
